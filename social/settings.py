6882e8473ec784ea94e5feae8c912f2c580187a4
Dayane Soares dos Anjos <adaurrea63@gmail.com>
2025-06-29 18:46:13 -0300

    adiciona domínio do Railway ao ALLOWED_HOSTS

--- social/settings.py
+++ social/settings.py
@@ -27,8 +27,7 @@ SECRET_KEY = 'django-insecure-ry_-s8mnz=o*uo7^i0z1)mnr9gs73l0jtr*fnj-rutqsdjkpvh
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
